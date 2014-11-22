16bb39320de9cb46e87b97a878f37c21b2f4c1d7
Bien Do <BienDo@biens-macbook-pro.local>
2014-11-22 13:59:21 +0700

    kk

--- CI_Test/ViewController.m
+++ CI_Test/ViewController.m
@@ -18,9 +18,6 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     NSString * aa =@"a";
-      NSString * aa =@"a";
-      NSString * aa =@"a";
-      NSString * aa =@"a";
     
 }
 
